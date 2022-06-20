b9a71046c7ab9f4539ec8dae5b9a814576496b3e
Danilo Gabriel Silvério <danilosilverio@outlook.com.br>
2022-06-20 19:24:37 -0300

    Design do aplicativo de Bate-Papo

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,11 +1,44 @@
 import React from "react";
-import { ChatApp, ContentApp, PageContent, Sidebar } from "../../assets/styles/Home/HomeStyle";
+
+// (STYLED-COMPONENTS)
+import { ChatApp, ContentApp, PageContent, Sidebar, SidebarHeader, SidebarHeaderButton, SidebarHeaderButtons, SidebarSearch } from "../../assets/styles/Home/HomeStyle";
+
+// (IMAGES)
+import AvatarIcon from "../../assets/images/avatar-icon.png";
+
+// (MATERIAL UI)
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
 
 const HomePage = () => {
     return (
         <PageContent>
             <ChatApp>
-                <Sidebar>...</Sidebar>
+                <Sidebar>
+                    <SidebarHeader>
+                        <img src={AvatarIcon} alt="Avatar Icon"/>
+                        <SidebarHeaderButtons>
+                            <SidebarHeaderButton>
+                                <DonutLargeIcon style={{color: "#f8fbff"}}/>
+                            </SidebarHeaderButton>
+                            <SidebarHeaderButton>
+                                <ChatIcon style={{color: "#f8fbff"}}/>
+                            </SidebarHeaderButton>
+                            <SidebarHeaderButton>
+                                <MoreVertIcon style={{color: "#f8fbff"}}/>
+                            </SidebarHeaderButton>
+                        </SidebarHeaderButtons>
+                    </SidebarHeader>
+                    <SidebarSearch>
+                        <div>
+                            <SearchIcon style={{color: "#919191"}}/>
+                            <input type="search" placeholder="Pesquisar ou começar uma nova conversa" />
+                        </div>
+                    </SidebarSearch>
+                    <div>contatos</div>
+                </Sidebar>
                 <ContentApp>...</ContentApp>
             </ChatApp>
         </PageContent>
